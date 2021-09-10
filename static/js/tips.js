64e43d24a75cfe1c4ef9df2f422ceedde8de79d3
liao895080264 <50914604+liao895080264@users.noreply.github.com>
2021-09-11 00:07:36 +0800

    fix:
    1. 修复重复添加图片问题
    
    2. 修复弹窗问题
    
    add:
    
    新增提示框

---
+++ static/js/tips.js
@@ -0,0 +1,34 @@
+//提示框
+function prompt(message, style, tip) {
+  style = (style === undefined) ? 'alert-success' : style;
+  let oDiv = $('<div>')
+      .appendTo('body')
+      .addClass('alert ' + style)
+      .html('<div style="font-size: 18px">' + tip + '</div>'+
+            '<div>' + message + '</div>')
+      .show()
+      .animate({top:35}, 500, function(){
+        setTimeout(function(){
+            oDiv.animate({opacity:0},500,function(){
+                oDiv.remove();
+            });
+        },1000);
+    })
+}
+
+// 成功提示
+function success_prompt(message, tip)
+{
+  prompt(message, 'alert-success', tip);
+}
+
+// 失败提示
+function fail_prompt(message, tip) {
+  prompt(message, 'alert-danger', tip);
+}
+
+// 提醒
+function warning_prompt(message, tip)
+{
+    prompt(message, 'alert-warning', tip);
+}
