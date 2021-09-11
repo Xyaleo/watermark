b2a708f1170f159ec01adb30293837b29a96aff3
liao895080264 <50914604+liao895080264@users.noreply.github.com>
2021-09-11 12:13:03 +0800

    add:
    后端增加返回图片类型
    
    fix:
    
    修复前端返回图片堆叠问题

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from predict_tool import remove_tool, preTest
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask import request
 
 app = Flask(__name__)
@@ -17,10 +17,13 @@ def getUpload():
     # 获取图片文件，设置图片文件name=1
     fileType = request.form.get("type")
     img = request.form.get("str")
-    print(fileType)
     # imagedata = base64.b64decode(img)  # 转码后得到的图片
     res = remove_tool([img], [fileType])
-    return res
+    json_dict = {
+        "res": res,
+        "fileType": fileType,
+    }
+    return jsonify(json_dict)
 
 
 if __name__ == '__main__':
