00b50c7d0243cb6f81c1561397c8185d6ae3a721
liao895080264 <50914604+liao895080264@users.noreply.github.com>
2021-09-11 11:36:09 +0800

    add:
    增加loading功能
    
    fix:
    
    前端代码格式化
    
    提示框样式优化
    
    修复图片太大导致的报错

--- static/js/tips.js
+++ static/js/tips.js
@@ -1,34 +1,35 @@
 //提示框
 function prompt(message, style, tip) {
-  style = (style === undefined) ? 'alert-success' : style;
-  let oDiv = $('<div>')
-      .appendTo('body')
-      .addClass('alert ' + style)
-      .html('<div style="font-size: 18px">' + tip + '</div>'+
+    if ($(".alert").length > 0) {
+        $(".alert").remove();
+    }
+    style = (style === undefined) ? 'alert-success' : style;
+    let oDiv = $('<div>')
+        .appendTo('body')
+        .addClass('alert ' + style)
+        .html('<div style="font-size: 18px">' + tip + '</div>' +
             '<div>' + message + '</div>')
-      .show()
-      .animate({top:35}, 500, function(){
-        setTimeout(function(){
-            oDiv.animate({opacity:0},500,function(){
-                oDiv.remove();
-            });
-        },1000);
-    })
+        .show()
+        .animate({top: 30}, 500, function () {
+            setTimeout(function () {
+                oDiv.animate({opacity: 0}, 500, function () {
+                    oDiv.remove();
+                });
+            }, 1000);
+        })
 }
 
 // 成功提示
-function success_prompt(message, tip)
-{
-  prompt(message, 'alert-success', tip);
+function success_prompt(message, tip) {
+    prompt(message, 'alert-success', tip);
 }
 
 // 失败提示
 function fail_prompt(message, tip) {
-  prompt(message, 'alert-danger', tip);
+    prompt(message, 'alert-danger', tip);
 }
 
 // 提醒
-function warning_prompt(message, tip)
-{
+function warning_prompt(message, tip) {
     prompt(message, 'alert-warning', tip);
 }
